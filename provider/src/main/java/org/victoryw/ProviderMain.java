25d088ab18f9d769e69707b8e64da1e186c0f000
tianyawy <tianya_wy@hotmail.com>
2016-12-14 21:19:20 +0800

    [victoryw] add the sub project provider

---
+++ provider/src/main/java/org/victoryw/ProviderMain.java
@@ -0,0 +1,13 @@
+package org.victoryw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderMain {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ProviderMain.class, args);
+    }
+
+}
