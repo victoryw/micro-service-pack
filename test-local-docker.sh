143844688bbfb6575b3f04a3ae411794b3f03aa1
tianyawy <tianya_wy@hotmail.com>
2017-02-03 23:04:37 +0800

    [victoryw] #n/a use docker compose to start service

--- test-local-docker.sh
+++ test-local-docker.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-curl -d '{"firstName":"Anne", "lastName":"Queen"}' -H 'Content-Type: application/json' http://localhost:8082/customers
-curl http://localhost:8082/customers/1
+curl -v  http://localhost:8082/customers/1 
+curl -v -d '{"firstName":"Anne", "lastName":"Queen"}' -H 'Content-Type: application/json' http://localhost:8082/customers
+curl -v http://localhost:8082/customers/1
