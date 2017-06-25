d6d8a2e0d8788065f77889784ef98f0f7336d6ca
tianyawy <tianya_wy@hotmail.com>
2017-06-25 12:14:32 +0800

    [victoryw] #n/a add the pitest, and i don't know how to;  add the debug mode

--- test-local-docker.sh
+++ test-local-docker.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-curl -v  http://localhost:8082/customers/1 
 curl -v -d '{"firstName":"Anne", "lastName":"Queen"}' -H 'Content-Type: application/json' http://localhost:8082/customers
-curl -v http://localhost:8082/customers/1
+curl -v  http://localhost:8082/customers/1 
